e7b90a4c7bce423aab27544c61fb0268cfba9857
yuuta-minede <_jamyukirom@gmail.com>
2022-07-24 18:31:34 +0900

    ownership and reference

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,8 @@ fn main() {
     }
     looping();
     range();
-    fibonacci(secret_number);
+    // fibonacci(secret_number);
+    ownership();
 }
 
 fn looping() {
@@ -70,10 +71,54 @@ fn fibonacci(x: u32) -> u32 {
     match x {
         0 => 0, // matchした時に fat arrow を使って戻り値を設定
         1 => 1,
+        2..=10 => 10,
         _ => {
-            let mut result: u32 = fibonacci(x - 1) + fibonacci(x - 2);
-            println!("{}", result);
-            result
+            let mut x: u32 = fibonacci(x - 1) + fibonacci(x - 2);
+            println!("x is {}", x);
+            x
         } // _ はその他
     }
+}
+
+fn ownership() {
+    // 文字列リテラルはコンパイル時に中身が判明していてサイズが分かっている
+    let mut s = "hello";
+    println!("s is {}", s);
+
+    // String型は不変かつ伸張可能なので、コンパイル時に不明な量のメモリをヒープ領域に確保する
+    // heap領域に確保されると、メモリは実行時にOSに要求されて、使用し終わったらOSに変換される
+    let mut t1 = String::from("hello");
+    t1.push_str(", world"); // 「hello」に「, world」を追加
+    println!("{}", t1);
+
+    // スタックにあるポインタ、長さ、許容量がコピーされる
+    // ポインタが指すヒープ上のデータ自体をコピーするわけではない
+    let t2 = t1;
+    println!("t2 is {}", t2);
+    // println!("{}", t1); // 二重開放エラーになる
+
+    let u1 = String::from("hey");
+    let u2 = u1.clone(); // deep copy すれば二重開放エラーにならない
+    println!("u1 = {}, u2 = {}", u1, u2);
+
+    let v1 = 5;
+    let v2 = v1; // deep copy
+    println!("v1 = {}, v2 = {}", v1, v2); // 整数ならコンパイル時に値が決まっているものはスタックに保存されるのでエラーにならない
+
+    let len = calculate_length(&t2); // & で借用することでt2の所有権はムーブされない
+    println!("The length of {} is {}", t2, len);
+
+    // let w = String::from("hoge");
+    // change(&w); // 参照しているものは変更はできない
+    let mut y = String::from("hoge");
+    change(&mut y);
+} // 最後に変数のスコープを外すようdrop関数を呼び出している
+
+fn calculate_length(s: &String) -> usize {
+    // 関数の引数で借用 ＝ 借用。所有権なし
+    s.len()
+}
+
+fn change(word: &mut String) {
+    word.push_str(", world"); // 参照しているものは変更はできないので引数をmutableにしている
 }
